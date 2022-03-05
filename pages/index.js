8101521205ffea2e7132e509f4809ecbd1a528e8
loaner <loaner@NL-SF-LOANER09.local>
2022-03-04 17:03:27 -0800

    changes

--- pages/index.js
+++ pages/index.js
@@ -13,7 +13,7 @@ export default function Home() {
 
       <main className={styles.main}>
         <h1 className={styles.title}>
-          Welcome to <a href="https://nextjs.org">Prerona's app!</a>
+          Welcome to <a href="https://nextjs.org">Preronas app!</a>
         </h1>
 
         <p className={styles.description}>
